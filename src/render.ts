f86dba484c3aaf4ddc862ba67b3b036fb5b8507f
Austin Hallock <austinhallock@gmail.com>
2022-06-23 21:17:22 -0600

    rm old code

--- src/render.ts
+++ src/render.ts
@@ -1,9 +1,6 @@
 import type { ReactElement } from "https://npm.tfl.dev/react";
 import type { RenderOptions } from "./types.ts";
-// import { createElement } from "https://npm.tfl.dev/react";
 import { LinkHeader } from "./links.ts";
-// import { SsrDataProvider } from "./react/useSsrData.ts";
-// import { StreamProvider } from "./react/useStream.ts";
 
 // FIXME: https://github.com/esm-dev/esm.sh/issues/78
 // context files are bundled (bad). so we need to roll with our own
@@ -15,17 +12,6 @@ export async function render(
 ) {
   const { strategy, bootstrapModules } = options;
 
-  // element = createElement(SsrDataProvider, null, element);
-
-  // // deno-lint-ignore prefer-const
-  // let injectToStream: (chunk: string) => void;
-
-  // element = createElement(
-  //   StreamProvider,
-  //   { value: { injectToStream: (chunk: string) => injectToStream(chunk) } },
-  //   element,
-  // );
-
   const stream = await renderToStream(element, {
     bootstrapModules,
     disable: strategy === "static",
@@ -36,7 +22,7 @@ export async function render(
   const links = new LinkHeader();
 
   for (const preload of bootstrapModules) {
-    if (preload.includes("//esm.sh") || preload.endsWith(".js")) {
+    if (preload.includes("//npm.tfl.dev") || preload.endsWith(".js")) {
       links.preloadModule(preload);
     }
   }
